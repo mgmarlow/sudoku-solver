3f75b1584fff6bdd4405865410de9db975daeda5
mgmarlow <mgmarlow@gmail.com>
2016-12-04 17:20:58 -0800

    Add solution benchmarking

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 import csv
 import sys
+import time
 
 def main():
     if len(sys.argv) != 2:
         print("Usage: maingit .py <file.csv>")
         exit(1)
 
-    grid = parsePuzzle(sys.argv[1])
-    for line in grid:
-        print(line)
+    start_time = time.time()
 
-def parsePuzzle(filename):
-    puzzleGrid = []
+    grid = parse_puzzle(sys.argv[1])
+    solution = solve(grid)
+
+    print("Solution found after %s" % (time.time() - start_time))
+    for row in solution:
+        print(row)
+
+def parse_puzzle(filename):
+    puzzle_grid = []
 
     try:
         with open(filename, newline='') as puzzleFile:
             reader = csv.reader(puzzleFile, delimiter=',')
             for row in reader:
-                puzzleGrid.append(list(map(lambda i: int(i), row)))
+                puzzle_grid.append(list(map(lambda i: int(i), row)))
     except IOError:
         print("There was an error opening the file specified.")
         exit(1)
 
-    return puzzleGrid
+    return puzzle_grid
+
+def solve(puzzle):
+    return 'stub'
 
 if __name__ == '__main__':
     main()
