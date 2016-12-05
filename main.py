201ca5255cac1c5c9d948f38b57e3c56e35b056d
mgmarlow <mgmarlow@gmail.com>
2016-12-04 17:11:20 -0800

    Read CSV file and parse into 2d int list

---
+++ main.py
@@ -0,0 +1,28 @@
+import csv
+import sys
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: main.py <file.csv>")
+        exit(1)
+
+    grid = parsePuzzle(sys.argv[1])
+    for line in grid:
+        print(line)
+
+def parsePuzzle(filename):
+    puzzleGrid = []
+
+    try:
+        with open(filename, newline='') as puzzleFile:
+            reader = csv.reader(puzzleFile, delimiter=',')
+            for row in reader:
+                puzzleGrid.append(list(map(lambda i: int(i), row)))
+    except IOError:
+        print("There was an error opening the file specified.")
+        exit(1)
+
+    return puzzleGrid
+
+if __name__ == '__main__':
+    main()
