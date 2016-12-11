2f425f264bcab0fa5d006c5dda21f8518fff2b11
mgmarlow <mgmarlow@gmail.com>
2016-12-10 16:26:33 -0800

    Add print tracepoints for debugging

--- src/algorithm.py
+++ src/algorithm.py
@@ -12,30 +12,44 @@ def solve(grid):
             for col_index in range(len(grid[row_index])):
                 if grid[row_index][col_index] == 0:
                     for new_val in range(1, 10):
-                        if only_possible_val(new_val, row_index, col_index, grid):
+                        if only_possible_val(new_val, row_index, grid):
+                            print('possible!')
                             grid[row_index][col_index] = new_val
                             break
 
-        print(solve_count)
+        #print(solve_count)
         if solve_count == 9:
             solved = True
 
     return grid
 
 # Returns True if only one integer can go in the given space
-def only_possible_val(test_val, row_index, col_index, grid):
-    #print("{%d, %d, %d}" % (row_index, col_index, val))
-    return 'stub'
+def only_possible_val(test_val, row_index, grid):
+    col_count = 0
+
+    for col_index in range(len(grid[row_index])):
+        # print("{%d, %d, %d}" % (row_index, col_index, test_val))
+        if proper_value(test_val, row_index, col_index, grid):
+            col_count += 1
+    #print(col_count)
+
+    return True if col_count == 1 else False
 
 # Return True if val (1-9) is valid, false if not
 def proper_value(val, row_index, col_index, grid):
+    # Check row
     if val in grid[row_index]:
         return False
+
+    # Check Column
     for row_list in grid:
         if row_list[col_index] == val:
             return False
+
+    # Check surrounding box
     if check_box(val, row_index, col_index, grid):
         return True
+
     return False
 
 def check_box(val, row_index, col_index, grid):
